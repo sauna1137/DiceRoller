e6ad6f7232b1bd9744cb3662f790686618a54b71
sauna1137 <subscription.search@gmail.com>
2023-04-11 08:08:34 +0900

    add countup

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,14 +5,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countButton: Button = findViewById(R.id.count_button)
+        countButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
@@ -21,5 +27,11 @@ class MainActivity : AppCompatActivity() {
         val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        count += 1
+        val countLabel: TextView = findViewById(R.id.count_label)
+        countLabel.text = count.toString()
     }
 }
