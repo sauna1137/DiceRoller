26d480e79df76827d179d0a792de25cec2acd357
sauna1137 <subscription.search@gmail.com>
2023-04-14 08:08:37 +0900

    add roll dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import org.w3c.dom.Text
@@ -19,6 +20,10 @@ class MainActivity : AppCompatActivity() {
 
         val countButton: Button = findViewById(R.id.count_button)
         countButton.setOnClickListener { countUp() }
+
+
+        val imageButton: Button = findViewById(R.id.image_button)
+        imageButton.setOnClickListener { rollDiceImage() }
     }
 
     private fun rollDice() {
@@ -34,4 +39,21 @@ class MainActivity : AppCompatActivity() {
         val countLabel: TextView = findViewById(R.id.count_label)
         countLabel.text = count.toString()
     }
+
+    private fun rollDiceImage() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+
+
 }
