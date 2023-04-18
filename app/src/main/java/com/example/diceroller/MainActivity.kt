7a4b4e964f84d8a913b110d8ee278bab691f49c2
sauna1137 <subscription.search@gmail.com>
2023-04-18 21:53:44 +0900

    add roll dice2

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.diceroller
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,6 +12,9 @@ import org.w3c.dom.Text
 class MainActivity : AppCompatActivity() {
 
     var count = 0
+    lateinit var diceImage : ImageView
+    lateinit var diceImage2: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,14 +25,11 @@ class MainActivity : AppCompatActivity() {
         val countButton: Button = findViewById(R.id.count_button)
         countButton.setOnClickListener { countUp() }
 
-
         val imageButton: Button = findViewById(R.id.image_button)
         imageButton.setOnClickListener { rollDiceImage() }
     }
 
     private fun rollDice() {
-//        Toast.makeText(this, "button clicked",
-//            Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
@@ -41,9 +42,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDiceImage() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
 
         val randomInt = (1..6).random()
+        val randomInt2 = (1..6).random()
+        
+        diceImage.setImageResource(getDiceImage(randomInt))
+        diceImage2.setImageResource(getDiceImage(randomInt2))
+    }
+    
+    private fun getDiceImage(randomInt: Int): Int {
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -52,7 +61,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
+        return drawableResource
     }
 
 
